c79f08a52a260af70c4eeb88a6e603c64c92e44a
boup <adamasow03@gmail.com>
2020-05-20 11:46:36 +0200

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,69 @@
+-----insert in city table
+insert into city values('Dakar','-17.467686°W','14.716677°N')
+
+insert into city values('Saint-Louis','16.4818°W','16.0326°N')
+
+insert into city values('Louga','16.2287° W','15.6142° N')
+
+insert into city values('Thies','16.9359° W','14.7910° N')
+
+insert into city values('Ziguinchor','16.2640° W','12.5641° N')
+
+insert into city values('Matam','13.2577° W','15.6600° N')
+
+insert into city values('Kaolack',' 16.0758° W','14.1652° N')
+
+insert into city values('Diourbel','16.2346° W','14.6561° N')
+
+insert into city values('Kedougou','12.1747° W','12.5605° N')
+
+insert into city values('Kolda','14.9506° W','12.9107° N')
+
+insert into city values('Tambacounda','13.6710° W','13.7726° N')
+
+insert into city values('Fatick','16.4111° W','14.3390° N')
+
+insert into city values('Kaffrine','15.5416° W','14.1052° N')
+
+insert into city values('Sédhiou','15.5562° W','12.7046° N')
+
+------insert in Line table---
+
+insert into line values('1,4,6,7,8,9,10,13,18,20,23,121,2,5,11,12,15,16A,16B,217,218,219,227,228,232','http://demdikk.com/ligne-13-liberte-5-palais-2/','iddakar')
+
+insert into line values('1,4,6,7,8,9,10,13,18,20,23,121,2,5,11,12,15,16A,16B,217,218,219,227,228,232','http://demdikk.com/ligne-13-liberte-5-palais-2/','idthies')
+
+insert into line values('1,4,6,7,8,9,10,13,18,20,23,121,2,5,11,12,15,16A,16B,217,218,219,227,228,232','http://demdikk.com/ligne-13-liberte-5-palais-2/','idsaintlouis')
+
+insert into line values('1,4,6,7,8,9,10,13,18,20,23,121,2,5,11,12,15,16A,16B,217,218,219,227,228,232','http://demdikk.com/ligne-13-liberte-5-palais-2/','idkaolack')
+
+insert into line values('1,4,6,7,8,9,10,13,18,20,23,121,2,5,11,12,15,16A,16B,217,218,219,227,228,232','http://demdikk.com/ligne-13-liberte-5-palais-2/','idsedhiou')
+
+insert into line values('1,4,6,7,8,9,10,13,18,20,23,121,2,5,11,12,15,16A,16B,217,218,219,227,228,232','http://demdikk.com/ligne-13-liberte-5-palais-2/','iddiourbel')
+
+------table operator-----
+
+insert into operator values('Dakar Dem Dkk','http://demdikk.com/','cityid')
+
+insert into operator values('Aftu','http://aftu-senegal.org/','cityid')
+
+insert into operator values('Car Rapide transport','https://carrapideprestige.business.site/?utm_source=gmb&utm_medium=referral','cityid')
+
+insert into operator values('Rapidos Car','https://courses.rapidosapp.com/','cityid')
+
+insert into operator values('Dakar Dem Dkk','http://demdikk.com/','cityid')
+
+-----table ticket----remark i will add price
+insert into ticekt values ('10min','price','zone1','cityid')
+
+insert into ticekt values ('20min','price','zone1','cityid')
+
+-----table  transcity---
+
+insert into transcity values('http://demdikk.com/senegal_dem_dikk/','Bus Senegal Dem Dikk','cityid')
+insert into transcity values('http://edreams.com/flights/dakar-ziguinchor/DKR/ZIG','Avion','cityid')
+insert into transcity values('http://edreams.com/flights/dakar-ziguinchor/DKR/ZIG','Avion','cityid')
+insert into transcity values('http://edreams.com/flights/dakar-ziguinchor/DKR/ZIG','Avion','cityid')
+
+
+1,4,6,7,8,9,10,13,18,20,23,121,2,5,11,12,15,16A,16B,217,218,219,227,228,232
