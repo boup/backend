fc09fb4bf75c87aea1bf079065906c203f87a95b
boup <adamasow03@gmail.com>
2020-05-25 12:57:46 +0200

    Add files via upload

--- script.sql
+++ script.sql
@@ -29,7 +29,7 @@ insert into city values('Sédhiou','15.5562° W','12.7046° N')
 
 ------insert in Line table---
 
-insert into line values('1,4,6,7,8,9,10,13,18,20,23,121,2,5,11,12,15,16A,16B,217,218,219,227,228,232','http://demdikk.com/ligne-13-liberte-5-palais-2/','iddakar')
+insert into line(link,number,cityid) values('1,4,6,7,8,9,10,13,18,20,23,121,2,5,11,12,15,16A,16B,217,218,219,227,228,232','http://demdikk.com/ligne-13-liberte-5-palais-2/','iddakar')
 
 insert into line values('1,4,6,7,8,9,10,13,18,20,23,121,2,5,11,12,15,16A,16B,217,218,219,227,228,232','http://demdikk.com/ligne-13-liberte-5-palais-2/','idthies')
 
@@ -43,20 +43,32 @@ insert into line values('1,4,6,7,8,9,10,13,18,20,23,121,2,5,11,12,15,16A,16B,217
 
 ------table operator-----
 
-insert into operator values('Dakar Dem Dkk','http://demdikk.com/','cityid')
+insert into operator(description,link,name_operator) values('Public Bus','http://demdikk.com/','Dakar Dem Dikk',1)
 
-insert into operator values('Aftu','http://aftu-senegal.org/','cityid')
+insert into operator(description,link,name_operator) values('Private Bus','http://aftu-senegal.org/','Aftu',1)
 
-insert into operator values('Car Rapide transport','https://carrapideprestige.business.site/?utm_source=gmb&utm_medium=referral','cityid')
+insert into operator(description,link,name_operator) values('Private Bus/Taxi','https://carrapideprestige.business.site/?utm_source=gmb&utm_medium=referral','Car Rapide transport',1)
 
-insert into operator values('Rapidos Car','https://courses.rapidosapp.com/','cityid')
+insert into operator(description,link,name_operator) values('Private Bus/Taxi','https://courses.rapidosapp.com/','Rapidos Car',1)
 
-insert into operator values('Dakar Dem Dkk','http://demdikk.com/','cityid')
+insert into operator(description,link,name_operator) values('Dakar Dem Dkk','http://demdikk.com/',1)
 
 -----table ticket----remark i will add price
-insert into ticekt values ('10min','price','zone1','cityid')
-
-insert into ticekt values ('20min','price','zone1','cityid')
+insert into ticket(duration,price,typeof_ticket,cityid) values ('End of section 1',150,'section 1',1)
+insert into ticket(duration,price,typeof_ticket,cityid) values ('End of section 2',175,'section 2',1)
+insert into ticket(duration,price,typeof_ticket,cityid) values ('End of section 3',200,'section 3',1)
+insert into ticket(duration,price,typeof_ticket,cityid) values ('End of section 4',250,'section 4',1)
+insert into ticket(duration,price,typeof_ticket,cityid) values ('End of section 5',300,'section 5',1)
+insert into ticket(duration,price,typeof_ticket,cityid) values ('End of sous/under section ',100,'sous/under section ',1)
+--insert into ticekt values ('20min',175,'zone1','cityid')
+----insert into travel bus--
+insert into travelbus(price,link) values(3500,'http://demdikk.com/senegal_dem_dikk/',1)
+insert into travelbus(price,link) values(6000,'http://rapido.com',1)
+insert into travelbus(price,link) values(7000,'http://senegaltours.com',1)
+---insert table travelfly
+insert into travelfly (price,company) values (70000,'http://edreams.com/flights/dakar-ziguinchor/DKR/ZIG',1)
+insert into travelfly (price,company) values (80000,'http://edreams.com/flights/dakar-ziguinchor/DKR/ZIG',1)
+insert into travelfly (price,company) values (100000,'http://edreams.com/flights/dakar-ziguinchor/DKR/ZIG',1)
 
 -----table  transcity---
 
