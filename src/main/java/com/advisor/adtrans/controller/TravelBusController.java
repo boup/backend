b6a24af4173213c25829dd6ff3d032a7f441a08d
sow <adamasow03@gmail.com>
2020-07-07 11:32:19 +0000

    new commit with controller and repository 7/5/2020 00:38 exception  addedd

--- src/main/java/com/advisor/adtrans/controller/TravelBusController.java
+++ src/main/java/com/advisor/adtrans/controller/TravelBusController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins="http://localhost:3000")
+
 public class TravelBusController {
     @Autowired
     private TravelBusRepository travelBusRepository;
